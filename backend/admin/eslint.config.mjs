435729a644509e3baa0e4e3dc3e49c80f7ecde82
NhanSamLe <ledaonhansam001@gmail.com>
2025-10-25 22:53:59 +0700

    commit

--- backend/admin/eslint.config.mjs
+++ backend/admin/eslint.config.mjs
@@ -32,7 +32,8 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       'prettier/prettier': 0,
-      "@typescript-eslint/no-unsafe-assignment": "off"
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unnecessary-type-assertion": "off"
     },
   },
 );
